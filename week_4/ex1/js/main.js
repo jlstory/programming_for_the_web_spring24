15fcf736c67c9e286c85b9ab4be3652a80a08376
jlstory <jlstory1@yahoo.com>
2024-02-05 16:16:16 -0600

    initial commit

--- week_4/ex1/js/main.js
+++ week_4/ex1/js/main.js
@@ -1,38 +1,15 @@
-function setup() {
-    createCanvas(200, 200);
-    background('white');
-    noLoop();
+//global vars
+const myEmptyDiv = document.querySelector('#myEmptyDiv');
+const heading = document.createElement('h1')
+// content
+heading.innerHTML = 'Llamas do not bite. They spit when they are agitated, but that is mostly at each other.';
+heading.style.cursor = 'pointer';   
+//event listener - click     
+heading.addEventListener('click', handleHeadingClick);
+//add h1
+myEmptyDiv.appendChild(heading);
+//onclick function
+function handleHeadingClick () {
+document.body.style.backgroundColor = 'indigo';
 }
 
-function createTile() {
-    translate(0, 0);
-    fill('black');
-    rect(0, 0, 200, 200);
-    stroke('white');
-    strokeWeight(5);
-    line(0, 0, 200, 200);
-    line(200, 0, 0, 200);
-    noStroke();
-    fill('orange');
-    circle(100, 25, 40);
-    circle(100, 175, 40);
-    circle(25, 100, 40);
-    circle(175, 100, 40);
-    fill('red');
-    circle(100, 50, 50);
-    circle(50, 100, 50);
-    circle(150, 100, 50);
-    circle(100, 150, 50);
-    noFill();
-    stroke('white');
-    strokeWeight(5);
-    circle(100, 100, 100);
-    circle(0, 0, 100);
-    circle(200, 0, 100);
-    circle(0, 200, 100);
-    circle(200, 200, 100);
-};
-
-function draw() {
-     createTile();
-}
