6129791c3522d5e0530262272e9d2ddd7fe4c527
jlstory <jlstory1@yahoo.com>
2024-02-09 16:53:59 -0600

    initial commit

---
+++ week_4/ex3/js/main.js
@@ -0,0 +1,40 @@
+// Global Vars
+let boxWidth = 150;
+let boxHeight = 200;
+
+//Cards Array
+let myCardsArray = [
+    // Row 1
+    {x: 225, y: 150},
+    {x: 425, y: 150},
+    {x: 625, y: 150},
+    {x: 825, y: 150},
+    {x: 1025, y: 150},
+    {x: 1225, y: 150}, 
+    // Row 2
+    {x: 225, y: 400},
+    {x: 425, y: 400},
+    {x: 625, y: 400},
+    {x: 825, y: 400},
+    {x: 1025, y: 400},
+    {x: 1225, y: 400},
+    // Row 3
+    {x: 225, y: 650},
+    {x: 425, y: 650},
+    {x: 625, y: 650},
+    {x: 825, y: 650},
+    {x: 1025, y: 650},
+    {x: 1225, y: 650},         
+]
+
+// Canvas
+function setup() {
+    createCanvas(1600, 900);
+    background(0, 0, 0, 0);
+}
+
+function draw() {
+    noLoop();
+    for(let i = 0; i < myCardsArray.length; i++)
+    rect(myCardsArray[i].x, myCardsArray[i].y, boxWidth, boxHeight)
+}
