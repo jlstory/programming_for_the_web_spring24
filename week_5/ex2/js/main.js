c4dc025ff22830f6a68107c5ecb7481d44c13d44
jlstory <jlstory1@yahoo.com>
2024-02-13 13:52:30 -0600

    comment code

--- week_5/ex2/js/main.js
+++ week_5/ex2/js/main.js
@@ -1,8 +1,9 @@
-// glocal vars
+// glocal vars & array
 let bubbles = [];
-let colorsArray = ['darkblue', 'blueviolet', 'navy', 'midnightblue', 'indigo', 'steelblue'];
 let randColor;
+let colorsArray = ['darkblue', 'blueviolet', 'navy', 'midnightblue', 'indigo', 'steelblue'];
 
+// canvas setup
 function setup() {
     createCanvas(600, 400);
         for (let i = 0; i < 20; i++) {
@@ -10,9 +11,9 @@ function setup() {
         let y = random(height);
         let r = random(10, 40);
         bubbles[i] = new Bubble(x, y, r);
+        }
     }
-    }
-
+// Draw function -> displays class
     function draw() {
         background(0, 0, 0, 5);       
         for (let i = 0; i < 20; i++) {
@@ -20,18 +21,19 @@ function setup() {
         bubbles[i].show();
         }
     }
-
+    // Class construction
     class Bubble {
     constructor(x, y, r) {
         this.x = x;
         this.y = y;
         this.r = r;
     }
+    // movement
     move() {
         this.x = this.x + random(-50, 50);
         this.y = this.y + random(-1, 1);
     }
-
+    // shape design
     show() {             
             randColor = random(colorsArray.length);
             randColor = floor(randColor);
