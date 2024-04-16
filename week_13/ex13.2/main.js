90b84b0f366a48ee78ac37eeed989d3a9050a555
jlstory <jlstory1@yahoo.com>
2024-04-16 13:40:44 -0500

    edits to css

--- week_13/ex13.2/main.js
+++ week_13/ex13.2/main.js
@@ -4,7 +4,6 @@ let i = 0;
 let white;
 let blue;
 
-
 function setup() { 
   createCanvas(windowWidth, windowHeight);
   frameRate(20);
@@ -13,23 +12,23 @@ function setup() {
 } 
 
 function draw() { 
-  background('rgba(0, 0, 0, .02)');
+  background('rgba(0, 0, 0, .03)');
   // color "loop" based on sine movement
   const color = map(sin(i), -1, 1, 0, 1);
   stroke(lerpColor(white, blue, color));
   strokeWeight(1)
-  noFill();
+  // noFill();
+  fill(lerpColor(white, blue, color))
   beginShape();
   vertex(0, height);
-  // for loop with sin() set up to control the y value of the vertex
+  // for loop with sin() set up to control the y value of the vertex (amplitude of the wave)
   for(var x = -5; x < width + 5; x++){
     let angle = offset + x * 0.01;
-    let y = map(sin(angle), -amp, amp, width / 2, height / 10);
+    let y = map(sin(angle), -amp, amp, width / 2, -height / 12);
     vertex(x, y);
   }
   vertex(width, height);
   endShape();
   offset += 0.1;
-  i += 0.01;
-  
+  i += 0.01; 
 }
