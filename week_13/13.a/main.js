a34d42dd0b30727fdd94d69fdfd1db9c03bbd4cb
jlstory <jlstory1@yahoo.com>
2024-04-16 19:49:57 -0500

    initial commit

---
+++ week_13/13.a/main.js
@@ -0,0 +1,137 @@
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+    // Fill the array with 0s
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = 0;
+    }
+  }
+  return arr;
+}
+
+// grid
+let grid;
+let velocityGrid;
+
+// size / hue / gravity
+let w = 2.5;
+let cols, rows;
+let hueValue = 180;
+let gravity = 0.15;
+
+// row is within the bounds
+function withinCols(i) {
+  return i >= 0 && i <= cols - 1;
+}
+
+// column is within the bounds
+function withinRows(j) {
+  return j >= 0 && j <= rows - 1;
+}
+
+function setup() {
+  createCanvas(600, 400);
+  colorMode(HSB, 360, 255, 255);
+  cols = width / w;
+  rows = height / w;
+  grid = make2DArray(cols, rows);
+  velocityGrid = make2DArray(cols, rows, 5);
+  frameRate(45);
+}
+
+function mouseDragged() {}
+
+function draw() {
+  background(0);
+
+  if (mouseIsPressed) {
+    let mouseCol = floor(mouseX / w);
+    let mouseRow = floor(mouseY / w);
+
+    // Randomly add sand particles
+    let matrix = 5;
+    let extent = floor(matrix / 2);
+    for (let i = -extent; i <= extent; i++) {
+      for (let j = -extent; j <= extent; j++) {
+        if (random(1) < 0.85) {
+          let col = mouseCol + i;
+          let row = mouseRow + j;
+          if (withinCols(col) && withinRows(row)) {
+            grid[col][row] = hueValue;
+            velocityGrid[col][row] = 1;
+          }
+        }
+      }
+    }
+    // Change the color of the sand over time
+    hueValue += 0.2;
+    if (hueValue > 360) {
+      hueValue = 1;
+    }
+  }
+
+  // sand particles
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      noStroke();
+      if (grid[i][j] > 0) {
+        fill(grid[i][j], 255, 255);
+        let x = i * w;
+        let y = j * w;
+        rect(x, y, w);
+      }
+    }
+  }
+
+  // frame 2
+  let nextGrid = make2DArray(cols, rows);
+  let nextVelocityGrid = make2DArray(cols, rows);
+
+  // Check cells for state value of 0 or 1
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      let velocity = velocityGrid[i][j];
+      let moved = false;
+      if (state > 0) {
+        let newPos = int(j + velocity);
+        for (let y = newPos; y > j; y--) {
+          let below = grid[i][y];
+          let dir = 1;
+          if (random(1) < 0.75) {
+            dir *= -1;
+          }
+          let belowA = -1;
+          let belowB = -1;
+          if (withinCols(i + dir)) belowA = grid[i + dir][y];
+          if (withinCols(i - dir)) belowB = grid[i - dir][y];
+
+          if (below === 0) {
+            nextGrid[i][y] = state;
+            nextVelocityGrid[i][y] = velocity + gravity;
+            moved = true;
+            break;
+          } else if (belowA === 0) {
+            nextGrid[i + dir][y] = state;
+            nextVelocityGrid[i + dir][y] = velocity + gravity;
+            moved = true;
+            break;
+          } else if (belowB === 0) {
+            nextGrid[i - dir][y] = state;
+            nextVelocityGrid[i - dir][y] = velocity + gravity;
+            moved = true;
+            break;
+          }
+        }
+      }
+
+      if (state > 0 && !moved) {
+        nextGrid[i][j] = grid[i][j];
+        nextVelocityGrid[i][j] = velocityGrid[i][j] + gravity;
+      }
+    }
+  }
+  grid = nextGrid;
+  velocityGrid = nextVelocityGrid;
+}
